beea4b249c08b94977710e7c68003c69ad40121a
Ojo Stephen <ojboy42@yahoo.com>
2018-03-23 03:18:01 +0100

    Update date entry UX'

--- src/InputEntry.js
+++ src/InputEntry.js
@@ -4,7 +4,7 @@ import { Field } from 'react-final-form';
 const InputEntry = ({label, type, name}) => 
 	<div className="form-group">
 		<label>{label}</label>
-	    <Field name={name} component="input" type={type} className="form-control col-12" />
+	    <Field name={name} component="input" type={type} className={`form-control col-${type==="date" ? 2: 12}`} />
 	</div>
 	
 export default InputEntry
