73cd055c1d2d39b28dfad4ac0c28767a42d42ea1
Ojo Stephen <ojboy42@yahoo.com>
2018-03-23 04:48:11 +0100

    Input width updated

--- src/SelectEntry.js
+++ src/SelectEntry.js
@@ -4,7 +4,7 @@ import { Field } from 'react-final-form';
 const SelectEntry = ({label, options, name}) =>
 	<div className="form-group">
 			<label>{label}</label>
-			<Field name={name} component="select" className="form-control col-2">
+			<Field name={name} component="select" className="form-control col-4">
 				{options.map((opt,  i) => 
 					  		<option value={opt.toLowerCase()} key={i}>{opt}</option>
 					  	)}
